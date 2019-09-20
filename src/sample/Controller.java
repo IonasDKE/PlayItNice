da80904b569b30ac4f06d06204262041d2bc1862
lougau92 <49477380+lougau92@users.noreply.github.com>
2019-09-20 22:02:19 +0200

    gui draft

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,19 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
+
 public class Controller {
+    @FXML
+    Slider Slider_Hight;
+    Slider Slider_Width;
+    TextField Text_Hight;
+    TextField Text_Width;
+
+    public void sliderPrint(ActionEvent e, TextField field, Slider slider){
+        field.setText(Double.toString((int)slider.getValue()));
+    }
+
 }
--- src/sample/Launcher.java
+++ src/sample/Launcher.java
@@ -1,3 +1,5 @@
+package sample;
+
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
