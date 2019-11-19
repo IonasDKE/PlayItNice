c9080f06922dc9e2851d37a6e112ecd3b73cd09e
Eliott Simon <43983606+EliottSimon17@users.noreply.github.com>
2019-11-19 12:12:26 +0100

    There is a null pointer that i can't debug

---
+++ src/AISolver.java
@@ -0,0 +1,23 @@
+package Model;
+
+import GameTree.State;
+import View.Board;
+import View.GraphicLine;
+import View.Line;
+import View.Player;
+
+public abstract class AISolver {
+
+    protected int playerColor;
+    protected Player getActualPlayer;
+    private final static int cScore = 20;
+    private final static int cThree = 15;
+    private final static int cTwo = 1;
+
+    protected int evaluationFunction(State board, int color){
+       //TODO
+        return 0;
+    }
+
+    public abstract Line nextMove(State board, int color);
+}
