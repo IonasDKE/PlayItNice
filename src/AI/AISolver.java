e38fd0beb18179be93603190798c45fa0d321b39
Eliott Simon <43983606+EliottSimon17@users.noreply.github.com>
2019-12-06 22:25:43 +0100

    added comments for both and evaluation function

--- src/AI/AISolver.java
+++ src/AI/AISolver.java
@@ -1,17 +1,10 @@
-
 package AI;
 
-import GameTree.Node;
 import GameTree.State;
-import GameTree.Tree;
-import View.Board;
-import View.Line;
-import View.Player;
+import View.*;
 
-import java.util.ArrayList;
-import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
-//import static Controller.Controller.completeSquareID;
 
 public abstract class AISolver {
 
@@ -20,17 +13,43 @@ public abstract class AISolver {
     private final static int cScore = 20;
     private final static int cThree = 15;
     private final static int cTwo = 1;
+    int counter =0;
 
-    protected int evaluationFunction(State board, int color){
+    /**
+     * This should work
+     * @param board
+     * @param color
+     * @return score of an evaluation function
+     */
+    public int evaluationFunction(State board, int color){
+        int score;
+        if(playerColor==1) {
+            score = cScore * board.getScore(0) - cScore * board.getScore(1);
+            //System.out.println("Red score");}
+        }
+        else{
+                //AI HERE
+                score = cScore * board.getScore(1) - cScore * board.getScore(0);
+                // System.out.println("Blue score "+ score) ;}
+            }
+        if(playerColor == color) {
+            System.out.println(counter + " board valence three: " + board.getValence(1));
+            counter++;
+            score += cThree * board.getValence(1) - cTwo * board.getValence(2);
+        }else
+            score -= cThree * board.getValence(1) - cTwo * board.getValence(2);
+        return score;
+    }
 
-        //return completeSquareID(State.currentState().getSquares());
-        Random r = new Random();
-        return r.nextInt(100-10)+10;
+    /**
+     * Creates a next move for the player
+     * @param board the state 'next move'
+     * @param color takes the turn ( which player is playing
+     */
+    public abstract Line nextMove(State board, int color);
 
+    public void setNewRoot(State state) {
 
     }
 
-    public void setNewRoots(State state){}
-
-    public abstract Line nextMove(State board, int color);
 }
--- src/AI/WeightedEdge.java
+++ src/AI/WeightedEdge.java
@@ -1,22 +1,34 @@
 package AI;
 
-import View.GraphicLine;
 import View.Line;
 
-public class WeightedEdge {
+public class WeightedEdge implements Comparable<WeightedEdge>{
 
     private int weight ;
     private Line line;
 
-    WeightedEdge(Line line, int weight) {
+    /**
+     *  This parameter takes in a line and a weight which is equals
+     *  to the evaluation function
+     *
+     * @param line takes in a  line
+     * @param weight and a weight
+     */
+    public WeightedEdge(Line line, int weight) {
         this.line = line ;
         this.weight = weight ;
     }
 
+    /**
+     * @return the line associated to a Weighted Edge
+     */
     public Line getLine(){
         return this.line;
     }
 
+    /**
+     * @return the score of a weighted edhe
+     */
     public int getWeight() {
         return weight;
     }
@@ -27,6 +39,15 @@ public class WeightedEdge {
 
     public void setLine(Line line){
         this.line = line;
+    }
+
+    /**
+     * @param edge another weighted edge
+     * @return the difference between the weights of two edges
+     */
+    @Override
+    public int compareTo(WeightedEdge edge) {
+        return this.weight - edge.weight;
     }
 }
 
