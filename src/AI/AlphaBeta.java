6537ca2a6567bc3a719669e14266112db8b1f77d
Eliott Simon <43983606+EliottSimon17@users.noreply.github.com>
2019-12-07 13:24:36 +0100

    alpha beta works.

--- src/AI/AlphaBeta.java
+++ src/AI/AlphaBeta.java
@@ -10,9 +10,10 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 
+
 public class AlphaBeta extends AISolver {
 
-   private static int maxDepth;
+    private static int maxDepth;
     final static int MIN = -1000000000, MAX = 1000000000;
     private long moveTime = 1900000000;
     private long startTime;
@@ -22,10 +23,10 @@ public class AlphaBeta extends AISolver {
         startTime = System.nanoTime();
         maxDepth = 1;
         Line line = null;
-        //board.display();
+        board.display();
+        //Starts at depth 0
         while (maxDepth <= board.numberOfAvailableMoves()) {
-
-            //Starts at depth 0
+            //System.out.println("moves : " + board.numberOfAvailableMoves());
             WeightedEdge weight = startAI(board, turn, 0, MIN, MAX);
             if((System.nanoTime() - startTime) < moveTime) {
                 line = weight.getLine();
@@ -36,7 +37,7 @@ public class AlphaBeta extends AISolver {
             maxDepth++;
 
         }
-       //State.findLine(line.getid()).fill();
+        //State.findLine(line.getid()).fill();
         //line.fill();
         return line;
 
@@ -45,63 +46,115 @@ public class AlphaBeta extends AISolver {
     public WeightedEdge startAI(State state, int turn, int depth, int alpha, int beta) {
 
         if ((depth < maxDepth && (System.nanoTime() - startTime) < moveTime)) {
+            ArrayList<Line> moves = state.getAvailableMoves();
             int availableMoves = state.numberOfAvailableMoves();
-            ArrayList<Line> moves = state.getEmptyLines();
-            //int movesLength = moves.size();
+            int movesLength = moves.size();
 
             if (availableMoves == 0) {
                 return new WeightedEdge(null, evaluationFunction(state, turn));
-
             }
-            //Collections.shuffle(moves);
+            Collections.shuffle(moves);
 
+            /**
+             * resets the state
+             */
+
+            ArrayList<State> childrenState = state.computeAndGetChildren();
+            WeightedEdge[] newEdges = new WeightedEdge[childrenState.size()];
+
+            for (int i = 0; i < childrenState.size(); i++) {
+                State newBoard = childrenState.get(i);
+                ArrayList<Line> stateLine = state.getLines();
+                Line line = State.findDiffLine(stateLine, childrenState.get(i).getLines());
+                newEdges[i] = new WeightedEdge(line, evaluationFunction(newBoard, (newBoard.getScore(turn) > state.getScore(turn) ? turn : State.inverseTurn(turn))));
+            }
+
+            Arrays.sort(newEdges);
+            moves = new ArrayList<Line>();
+            if (playerColor != turn) {
+                for (int i = 0; i < childrenState.size(); i++) {
+                    moves.add(newEdges[i].getLine());
+                }
+            }
+            else{
+                for (int i = childrenState.size() - 1; i >= 0; i--)
+                    moves.add(newEdges[i].getLine());
+            }
             // IF TURN = AI
-            if (state.getActualPlayer().isAlpha()) {
-                //System.out.println("is AI");
+            if (turn == playerColor) {
                 // This is the edge we will return
                 WeightedEdge newEdge = new WeightedEdge(null, MIN);
 
+                //computes the children if they do not exist
+                //for (State child : state.computeAndGetChildren()) {
+                for(int i = 0; i < moves.size(); i ++){
+                    State child = state.computeAChild(moves.get(i));
+                    //Training edge
+                    WeightedEdge wedge;
 
-                     //computes the children if they do not exist
-                    for (State child : state.computeAndGetChildren()) {
-                        //State child = state.updateState(moves.get(i), color);
-                        //Training edge
-                        WeightedEdge wedge;
+                    int childScore = child.getScore(state.getActualPlayer());
+                    int actualScore = state.getScore(state.getActualPlayer());
+                    boolean found = false;
+                    if (childScore == actualScore) {
+                        wedge = startAI(child, 0, depth + 1, alpha, beta);
+                        found = true;
+                    } else {
+                        wedge = startAI(child, turn, depth + 1, alpha, beta);
+                    }
 
-                        int childScore = child.getScore(state.getActualPlayer());
-                        int actualScore = state.getScore(state.getActualPlayer());
-                        boolean found = false;
-                        if (childScore == actualScore) {
-                            wedge = startAI(child, 0, depth + 1, alpha, beta);
-                            found = true;
-                        } else {
-                            wedge = startAI(child, turn, depth + 1, alpha, beta);
-                        }
+                    int getScore = wedge.getWeight();
+                    // Backtracks
+                    if (newEdge.getWeight() < getScore) {
+                        newEdge.setWeight(getScore);
+                        newEdge.setLine(State.findDiffLine(state.getLines(), child.getLines()));
+                    }
+                    if (found)
+                        if (getScore >= beta)
+                            return newEdge;
 
-                        int getScore = wedge.getWeight();
-                        // Backtracks
-                        if (newEdge.getWeight() < getScore) {
-                            newEdge.setWeight(getScore);
-                            newEdge.setLine(State.findDiffLine(state.getLines(), child.getLines()));
-                        }
-                        if (found)
-                            if (getScore >= beta)
-                                return newEdge;
-
-                        alpha = Math.max(alpha, newEdge.getWeight());
+                    alpha = Math.max(alpha, newEdge.getWeight());
 
                 }
                 //System.out.println(newEdge.getLine().getid());
                 //State.findLine(newEdge.getLine().getid()).fill();
                 return newEdge;
             } else {
+                /*WeightedEdge newEdge = new WeightedEdge(null, MAX);
 
-            WeightedEdge newEdge = new WeightedEdge(null, MAX);
+                for (int i = 0; i < moves.size(); i++) {
+
+                    State child = state.computeAndGetChildren().get(i);
+                    WeightedEdge pair;
+                    int childScore = child.getScore(turn), currentScore = state.getScore(turn);
+                    boolean flag = false;
+                    if (childScore == currentScore) {
+                        pair = startAI(child, State.inverseTurn(turn), depth+1, alpha, beta);
+                        flag = true;
+                    } else
+                        pair = startAI(child, turn, depth+1, alpha, beta);
+
+                    int childUtility = pair.getWeight();
+                    if (newEdge.getWeight() > childUtility) {
+                        newEdge.setWeight(childUtility);
+                        newEdge.setLine(State.findDiffLine(state.getLines(), child.getLines()));
+                    }
+                    if (flag)
+                        if (childUtility <= alpha)
+                            return newEdge;
+
+                    beta = Math.min(beta, newEdge.getWeight());
+                }
+                return newEdge;
+            }
+                 */
+                WeightedEdge newEdge = new WeightedEdge(null, MAX);
 
                 //System.out.println("is not ai");
 
                 //computes the children if they do not exist
-                for (State child : state.computeAndGetChildren()) {
+               // for (State child : state.computeAndGetChildren()) {
+                for(int i = 0; i < moves.size(); i ++){
+                    State child = state.computeAChild(moves.get(i));
 
                     //State child = state.updateState(moves.get(i), color);
                     //Training edge
@@ -111,6 +164,7 @@ public class AlphaBeta extends AISolver {
                     int childScore = child.getScore(p);
                     int actualScore = state.getScore(p);
                     boolean found = false;
+
 
                     if (childScore == actualScore) {
                         wedge = startAI(child, 0, depth + 1, alpha, beta);
@@ -132,19 +186,19 @@ public class AlphaBeta extends AISolver {
                     beta = Math.min(beta, newEdge.getWeight());
                 }
 
-            //System.out.println(newEdge.getLine().getid());
+                //System.out.println(newEdge.getLine().getid());
 
-            //State.findLine(newEdge.getLine().getid()).fill();
-            return newEdge;
+                //State.findLine(newEdge.getLine().getid()).fill();
+                return newEdge;
 
-         }
+
+            }
         }
 
-     else {
-        // System.out.println("blablabla");
-        return new WeightedEdge(null, evaluationFunction(state, turn));
+        else{
+                // System.out.println("blablabla");
+                return new WeightedEdge(null, evaluationFunction(state, turn));
+            }
         }
-    }
 
-    public void setNewRoots(State state) {}
 }
