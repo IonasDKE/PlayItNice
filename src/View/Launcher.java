55021b0e3d053660e2d32061419459fd9603318c
Eliott Simon <43983606+EliottSimon17@users.noreply.github.com>
2019-12-07 13:14:11 +0100

    added minimax

--- src/View/Launcher.java
+++ src/View/Launcher.java
@@ -31,8 +31,8 @@ public class Launcher  extends Application {
     private ComboBox selectPlayerOne, selectPlayerTwo , numberOfPlayers;
     private String[] gameSizes = {"2 x 2", "3 x 3" , "4 x 4", "4 x 5", "5 x 6", "8 x 8"};
     //Creates the label for all the types of player
-    private ObservableList<String> typeOfPlayerOne = FXCollections.observableArrayList("Opponent 1", "Human", "Rule Based","Alpha Beta", "Mcts");
-    private ObservableList<String> typeOfPlayerTwo = FXCollections.observableArrayList("Opponent 2", "Human", "Rule Based","Alpha Beta", "Mcts");
+    private ObservableList<String> typeOfPlayerOne = FXCollections.observableArrayList("Opponent 1", "Human", "Rule Based","Alpha Beta", "Mcts", "MiniMax");
+    private ObservableList<String> typeOfPlayerTwo = FXCollections.observableArrayList("Opponent 2", "Human", "Rule Based","Alpha Beta", "Mcts", "MiniMax");
     private ObservableList<String> playerNumbers = FXCollections.observableArrayList("Select a number","1","2");
     private RadioButton[] radioButtons;
     private GridPane sizeBox;
